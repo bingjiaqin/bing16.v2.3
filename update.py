822aca42293cb26f4ea35f592191eac820ffa7ca
qbj <bingjiaqin@foxmail.com>
2021-07-17 18:15:18 +0800

    .

--- update.py
+++ update.py
@@ -1,5 +1,8 @@
 import os
+import sys
 
-os.system("git add .")
-os.system("git commit -a -m \".\"")
-os.system("git push origin master")
+if __name__ == '__main__':
+    msg = sys.argv[1]
+    os.system("git add .")
+    os.system("git commit -a -m \".\"")
+    os.system("git push origin master")
