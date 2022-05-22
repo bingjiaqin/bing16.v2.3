093654572f950d5ef511941df833105a1838c696
qbj <bingjiaqin@foxmail.com>
2022-05-22 12:01:57 +0800

    .

--- utils/header.js
+++ utils/header.js
@@ -3,12 +3,12 @@ const headerForm={
             return {
                 "hide": true,
                 "navs": [
-                { "title": "主页", "style": {"width": "72px"}, "href": "index.html"},
-                { "title": "乌有之乡", "style": {"width": "100px"}, "href": "wyzx.html"},
-                { "title": "随思乱想", "style": {"width": "100px"}, "href": "sslx.html"},
-                { "title": "浮光掠影", "style": {"width": "100px"}, "href": "fgly.html"},
-                { "title": "|", style: {width: "20px"}},
-                { "title": "关于我", style: {width: "70px"}, href: "about.html"}
+                { "title": "主页", "style": {"width": "100%", "min-width": "72px"}, "href": "index.html"},
+                { "title": "乌有之乡", "style": {"width": "100%", "min-width": "100px"}, "href": "wyzx.html"},
+                { "title": "随思乱想", "style": {"width": "100%", "min-width": "100px"}, "href": "sslx.html"},
+                { "title": "浮光掠影", "style": {"width": "100%", "min-width": "100px"}, "href": "fgly.html"},
+                { "title": "|", style: {"width": "100%", "min-width": "20px"}},
+                { "title": "关于我", style: {"width": "100%", "min-width": "70px"}, href: "about.html"}
                 ]
             }
         },
@@ -29,16 +29,16 @@ const headerForm={
         <ul class="NavUl">\
             <span v-for="nav in navs" class="NavList" >\
                 <span v-if="nav.title==pageTitle">\
-                    <li v-bind:style="nav.style">\
+                    <li class="NavLi" v-bind:style="nav.style">\
                         <span class="rcolor"><span class="divcss5-x2-header" @click="click()">{{ nav.title }}</span>\
                     </li>\
                 </span>\
                 <span v-else v-show="!mobile_flag || !hide">\
-                    <li v-bind:style="nav.style"><a v-bind:href="nav.href">{{ nav.title }}</a></li>\
+                    <li class="NavLi" v-bind:style="nav.style"><a v-bind:href="nav.href">{{ nav.title }}</a></li>\
                 </span>\
             </span>\
             <span class="NavList" v-show="!mobile_flag || !hide">\
-                <li class="NavList" style="width: 40px">\
+                <li class="NavLi" style="width: 40px">\
                     <a href="https://github.com/bingjiaqin"><img src="images/github.jpg" width="19px" height="19px" style="margin-top: 3px"></a>\
                 </li>\
             </span>\
